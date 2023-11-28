a72f370384179d53cf20a8307cdf12dd51d34c4a
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-29 00:33:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //app create 
 const express = require("express");
 const app = express();
@@ -14,4 +15,25 @@ app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
 }));
+
+
+//DB Connect
+const db = require("./config/database");
+db.dbConnect();
+
+
+// cloud connect
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudConnect();
+
+
+// api route mount
+const Upload = require("./routes/FileUpLoad");
+app.use("/api/v1/upload", Upload);
+
+
+//server activate
+app.listen(PORT, () => {
+    console.log(`Server started successfully at Port no: ${PORT}`);
+})
 
