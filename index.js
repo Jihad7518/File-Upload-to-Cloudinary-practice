cf18f757c326dbf65fd3d6fd8438bb36b24393a7
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-29 00:33:47 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+//app create 
+const express = require("express");
+const app = express();
+
+//find port 
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+//middleware
+app.use(express.json());
+//express file upload function
+const fileUpload = require("express-fileupload");
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
 
