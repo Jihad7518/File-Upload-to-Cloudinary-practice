798ffdc11b344a03006be93df9f2aa979325f17e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-30 04:06:06 +0600

    Update File.js

--- models/File.js
+++ models/File.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+
+const fileSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+    },
+    tags: {
+        type: String,
+    }, 
+    email: {
+        type: String,
+    }
+});
 
